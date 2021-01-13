da7e816b04b833cc809cf70bfd07e98b6dc69e3e
dratech <dratech92@gmail.com>
2021-01-13 09:36:06 +0100

    Added project skeleton

---
+++ src/main/java/com/tlac/rsocketfiddle/RsocketFiddleApplication.java
@@ -0,0 +1,13 @@
+package com.tlac.rsocketfiddle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsocketFiddleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsocketFiddleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tlac/rsocketfiddle/RsocketFiddleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlac.rsocketfiddle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsocketFiddleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
